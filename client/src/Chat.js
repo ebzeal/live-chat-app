625d649f80c03f734832c088c7ecf97cc233ab24
olu <ebenezerajayi@gmail.com>
2020-10-22 04:31:32 +0100

    49. The useSubscription Hook

--- client/src/Chat.js
+++ client/src/Chat.js
@@ -1,17 +1,24 @@
-import React from 'react';
-import { useQuery, useMutation } from '@apollo/react-hooks';
-import { messagesQuery, addMessageMutation } from './graphql/queries';
+import React, {useState} from 'react';
+import { useQuery, useMutation, useSubscription } from '@apollo/react-hooks';
+import { messagesQuery, addMessageMutation, messageAddedSubscription } from './graphql/queries';
 import MessageInput from './MessageInput';
 import MessageList from './MessageList';
 
 const Chat = ({user}) => {
-  const result = useQuery(messagesQuery);
+  const [messages, setMessages] = useState([]);
+  useQuery(messagesQuery, {
+    onCompleted: (data) => setMessages(data.messages)
+  });
   // const  {data, loading, error} = result;
-  const  {data} = result;
+  // const  {data} = result;
+  useSubscription(messageAddedSubscription, {
+    onSubscriptionData: ({subscriptionData: {data}}) => { // destructure result down into data
+    setMessages(messages.concat(data.messageAdded));
+    }
+  });
   // const [addMessage, {loading, error, data, called}] = useMutation(addMessageMutation) // useMutation returns an array
   const [addMessage] = useMutation(addMessageMutation) // useMutation returns an array
   // console.log("Chat -> result", result)
-  const messages = data?.messages ?? [];
 
 const handleSend = async (text) => {
   await addMessage({variables: {input: {text}}})
