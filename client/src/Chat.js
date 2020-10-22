151dc053a45025793f44ba4c4997536235f26b84
olu <ebenezerajayi@gmail.com>
2020-10-22 04:09:05 +0100

    48. The useMutation Hook

--- client/src/Chat.js
+++ client/src/Chat.js
@@ -1,17 +1,21 @@
 import React from 'react';
-import { useQuery } from '@apollo/react-hooks';
-import { messagesQuery } from './graphql/queries';
+import { useQuery, useMutation } from '@apollo/react-hooks';
+import { messagesQuery, addMessageMutation } from './graphql/queries';
 import MessageInput from './MessageInput';
 import MessageList from './MessageList';
 
 const Chat = ({user}) => {
-  const result = useQuery(messagesQuery)
-  console.log("Chat -> result", result)
-  const  {data} = result
+  const result = useQuery(messagesQuery);
+  // const  {data, loading, error} = result;
+  const  {data} = result;
+  // const [addMessage, {loading, error, data, called}] = useMutation(addMessageMutation) // useMutation returns an array
+  const [addMessage] = useMutation(addMessageMutation) // useMutation returns an array
+  // console.log("Chat -> result", result)
   const messages = data?.messages ?? [];
 
-const handleSend = (text) => {
-  // Todo
+const handleSend = async (text) => {
+  await addMessage({variables: {input: {text}}})
+  // console.log("handleSend -> data", data)
   }
   return (
     <section className="section">
